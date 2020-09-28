c81fdf3e720ea1377f2d2f8629a4ca91f7fda1f1
dreamP <2374995782@qq.com>
2020-09-28 22:23:35 +0800

    第三次提交，完成首页大部分效果

--- happigo/src/script/index_module.js
+++ happigo/src/script/index_module.js
@@ -11,9 +11,11 @@ define([], function() {
                     $.each(data, function(index, value) {
                         strhtml += `
                     <li>
+                    <a href="#">
                         <img src="${value.url}">
                         <p>${value.title}</p>
                         <span>￥${value.price}</span>
+                        </a>
                     </li>
                     `;
                     });
@@ -43,8 +45,62 @@ define([], function() {
                     $('.sail_list').html(strhtml);
                 })
             }();
+            //二级菜单栏
+            let $header_f_main = $('.header_f_main .a1');
+            let $menu = $('#menu');
+            let $menuli = $('#menu li');
+            let $hezi = $('.two_hz'); //右边的内容框
+            let $item = $('.two_hz .item');
+            console.log($hezi);
+            //鼠标移入"所有分类"显示菜单
+            $($header_f_main).on('mouseover', function() {
+                $($menu).show();
+                $($hezi).hide();
+            });
+            //鼠标移入菜单让菜单自己显示
+            $($menu).on('mouseover', function() {
+                $($menu).show();
+            });
+            $($menuli).on('mouseover', function() {
+                $(this).addClass('active').siblings().removeClass('active');
+                $item.eq($(this).index()).show().siblings('.item').hide(); //当前和li匹配的item显示，其他的隐藏
+                $hezi.show();
+            });
+            $($hezi).on('mouseover', function() {
+                $hezi.show();
+            });
+            $($menuli).on('mouseout', function() {
+                // $($hezi).hide();
+                $($menu).hide();
+            });
+
+
+            //tab切换
+            let $week_li = $('.week_li');
+            let $week_he = $('.week_he');
+            let $kuai = $('.sh_list ul li');
+            console.log($kuai);
+            //添加点击事件
+            $week_li.on('click', function() {
+                $(this).css({
+                    border: '2px solid #df0010',
+                    borderBottom: '2px solid #f5f5f5',
+                    color: '#c4193f',
+                    fontWeight: 'bold'
+                }).siblings('li').css({
+                    border: 'none',
+                    fontWeight: 'normal',
+                    color: 'green'
+                });
+                $(this).addClass('active').siblings('div').removeClass('active');
+                $kuai.eq($(this).index()).show().siblings('div').hide();
+            });
+            $week_li.on('mouseover', function() {
+                $week_li.css({ cursor: 'pointer' })
+            })
 
         }
     }
+
 
 });
