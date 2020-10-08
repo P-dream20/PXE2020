2435a593103a68d5b1160a8eb0fc5daf6ad54e1b
dreamP <2374995782@qq.com>
2020-10-08 20:58:15 +0800

    所有页面

--- happigo/src/script/index_module.js
+++ happigo/src/script/index_module.js
@@ -59,9 +59,47 @@ define([], function() {
                     });
                 })
             });
-            //二级菜单栏
-            $(function() {
 
+            //头部
+            $(function() {
+                    const $li5 = $('.li5');
+                    const $li6 = $('.li6');
+                    const $li_wei = $('.li_wei');
+                    const $help = $('.help');
+                    $li5.on('mouseover', function() {
+                        $li_wei.show();
+
+                    })
+                    $li_wei.on('mouseover', function() {
+                        $li_wei.show();
+                    })
+                    $li_wei.on('mouseout', function() {
+                        $li_wei.hide();
+                    })
+
+                    $li6.on('mouseover', function() {
+                        $help.show();
+                        $li6.css('border-color', '#ccc');
+                        $li6.css('background-color', '#fff');
+                    })
+                    $help.on('mouseover', function() {
+                        $help.show();
+                        $li6.css('border-color', '#ccc');
+                        $li6.css('background-color', '#fff');
+                    })
+                    $help.on('mouseout', function() {
+                        $help.hide();
+                        $li6.css('background-color', '#f5f5f5');
+                        $li6.css('border-color', '#f5f5f5');
+                    })
+                    $li6.on('mouseout', function() {
+                        $help.hide();
+                        $li6.css('background-color', '#f5f5f5');
+                        $li6.css('border-color', '#f5f5f5');
+                    })
+                })
+                //二级菜单栏
+            $(function() {
                     let $header_f_main = $('.header_f_main .a1');
                     let $menu = $('#menu');
                     let $menuli = $('#menu li');
@@ -69,25 +107,28 @@ define([], function() {
                     let $item = $('.two_hz .item');
                     console.log($hezi);
                     //鼠标移入"所有分类"显示菜单
-                    $($header_f_main).on('mouseover', function() {
-                        $($menu).show();
-                        $($hezi).hide();
+                    $header_f_main.on('mouseover', function() {
+                        $menu.show();
+                        $hezi.hide();
+                        $menuli.removeClass('active');
+                    });
+                    $header_f_main.on('mouseout', function() {
+                        $menu.hide();
                     });
                     //鼠标移入菜单让菜单自己显示
-                    $($menu).on('mouseover', function() {
-                        $($menu).show();
+                    $menu.on('mouseover', function() {
+                        $menu.show();
                     });
-                    $($menuli).on('mouseover', function() {
+                    $menuli.on('mouseover', function() {
                         $(this).addClass('active').siblings().removeClass('active');
                         $item.eq($(this).index()).show().siblings('.item').hide(); //当前和li匹配的item显示，其他的隐藏
                         $hezi.show();
                     });
-                    $($hezi).on('mouseover', function() {
+                    $hezi.on('mouseover', function() {
                         $hezi.show();
                     });
-                    $($menuli).on('mouseout', function() {
-
-                        $($menu).hide();
+                    $menuli.on('mouseout', function() {
+                        $menu.hide();
                     });
                     $hezi.on('mouseout', function() {
                         $menu.hide();
@@ -110,6 +151,59 @@ define([], function() {
                 })
             })
 
+            //顶部悬浮
+            $(function() {
+                const $nav = $('.week_ul');
+                console.log($nav);
+                //触发滚轮事件
+                $(window).on('scroll', function() {
+                    let $top = $(window).scrollTop(); //获取滚动条的top值。
+                    if ($top >= 400) {
+                        $nav.stop(true).animate({
+                            top: 0
+                        });
+                    } else {
+                        $nav.stop(true).animate({
+                            top: -60
+                        });
+                    }
+                });
+            })
+
+            //手机下单后面的二维码
+            $(function() {
+                const $iphone = $('.iphone');
+                const $iphone_wei = $('.iphone_wei');
+                $iphone_wei.hide();
+                const $iphone1 = $('.iphone1');
+                const $iphone_wei1 = $('.iphone_wei1');
+                $iphone_wei1.hide();
+                $iphone.on('mouseover', function() {
+                    $iphone_wei.show();
+                });
+                $iphone_wei.on('mouseover', function() {
+                    $iphone_wei.show();
+                });
+                $iphone_wei.on('mouseout', function() {
+                    $iphone_wei.hide();
+                });
+                $iphone.on('mouseout', function() {
+                    $iphone_wei.hide();
+                });
+                //右边
+                $iphone1.on('mouseover', function() {
+                    $iphone_wei1.show();
+                });
+                $iphone_wei1.on('mouseover', function() {
+                    $iphone_wei1.show();
+                });
+                $iphone_wei1.on('mouseout', function() {
+                    $iphone_wei1.hide();
+                });
+                $iphone1.on('mouseout', function() {
+                    $iphone_wei1.hide();
+                });
+            })
 
         }
     }
--- happigo/src/script/login_module.js
+++ happigo/src/script/login_module.js
@@ -1,38 +1,85 @@
 define([], function() {
     return {
         init: function() {
-            $.ajax({
-                url: 'http://localhost/PXE2020/happigo/php/registry.php',
-                dataType: 'json'
-            }).done(function(d) {
-                console.log(d);
-            })
-            const $tel = $('.tel');
-            const $yzm = $('.yzm');
-            const $span1 = $('.sp_1');
-            const $span2 = $('.sp_2');
-            $tel.on('focus', function() {
-                $span1.html($(this).html() + "请输入手机号码");
-                $span1.css('color', ' rgb(140, 140, 140)');
-            })
-            $tel.on('blur', function() {
-                if ($(this).value === '') {
-                    $span1.html($(this).html() + "您输入号码有误");
-                    $span1.css('color', ' rgb(196, 32, 59)');
-                }
-            })
+            //     $.ajax({
+            //         // url: 'http://192.168.13.10/PXE2020/happigo/php/registry.php',
+            //         url: 'http://localhost/PXE2020/happigo/php/login.php',
+            //         dataType: 'json'
+            //     }).done(function(d) {
+            //         console.log(d);
+            //     })
+            //     const $tel = $('.tel');
+            //     const $yzm = $('.yzm');
+            //     const $span1 = $('.sp_1');
+            //     const $span2 = $('.sp_2');
+            //     const $form = $('form');
+            //     const $submit = $('.submit');
+            //     let $telflag = true;
+            //     let $yzmflag = true;
+            //     $tel.on('focus', function() {
+            //         $span1.html($(this).html() + "请输入手机号码");
+            //         $span1.css('color', ' rgb(140, 140, 140)');
+            //     })
+            //     $tel.on('blur', function() {
+            //         if ($(this).value !== '') {
+            //             // $span1.html($(this).html() + "您输入号码有误");
+            //             // $span1.css('color', ' rgb(196, 32, 59)');
+            //             var reg = /^1[345789]\d{9}$/;
+            //             if (reg.test(this.value)) {
+            //                 $telflag = true;
+            //                 $span1.html(" ");
+            //             } else {
+            //                 $span1.html("您输入号码有误");
+            //                 $span1.css('color', ' rgb(196, 32, 59)');
+            //             }
+            //         } else {
+            //             $span1.html("您输入号码有误");
+            //             $span1.css('color', ' rgb(196, 32, 59)');
+            //         }
+            //     })
 
-            //短信验证码
-            $yzm.on('focus', function() {
-                $span2.html($(this).html() + "请输入您的短信验证码");
-                $span2.css('color', ' rgb(140, 140, 140)');
-            })
-            $yzm.on('blur', function() {
-                if ($(this).value === '') {
-                    $span2.html($(this).html() + "请输入您的短信验证码错误");
-                    $span2.css('color', ' rgb(140, 140, 140)');
-                }
-            })
+            //     //短信验证码
+            //     $yzm.on('focus', function() {
+            //         $span2.html("请输入您的短信验证码");
+            //         $span2.css('color', ' rgb(140, 140, 140)');
+            //     });
+            //     $yzm.on('blur', function() {
+            //         if ($(this).value !== '') {
+            //             var reg = /^1[345789]\d{2}$/;
+            //             $span2.html(" ");
+            //             $yzmflag = true;
+            //         }
+            //     });
+            //     $form.on('submit' , function() {
+            //         if ($tel.value !== '') {
+            //             $telflag = false;
+            //         }
+            //         if ($yzm.value !== '') {
+            //             $yzmflag = false;
+            //         }
+            //         if (!$tel || !$yzm) {
+            //             return false;
+            //         }
+            //     })
+            $('.suubmit').on('click', function() {
+                $.ajax({
+                    type: 'post',
+                    url: 'http://localhost/PXE2020/happigo/php/login.php',
+                    data: {
+                        user: $('.tel').val(),
+                        pass: hex_sha1($('.yzm').val())
+                    }
+                }).done(function(result) {
+                    if (result) {
+                        location.href = "index1.html";
+                        localStorage.setItem('tel', $('.tel').val());
+                    } else {
+                        $('.yzm').val('');
+                        alert('用户名或者密码错误');
+                    }
+                });
+            });
+
         }
 
     }
